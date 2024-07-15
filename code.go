e6f742b835d8e999b50ad86c076a159a201be4be
Andrei Siniavskij <siniavskij.andrei@yandex.ru>
2024-07-15 13:56:22 +0300

    change code

--- code.go
+++ code.go
@@ -1,30 +1,35 @@
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	s := "Hello, World!"
+	s := "Hello, John!"
+
+	num := 9
 
 	fmt.Println(s)
 	fmt.Println(s)
 	fmt.Println(s)
+
+	sqrt := math.sqrt(num)
+	fmt.Println(sqrt)
 
 	fmt.Println(`
-		a1
-		a2
-		a3
+		d1
+		d2
+		d3
 
 
 
 		b1
 		b2
-		b3
 
 
 
-		c1
-		c2
-		c3
+		e1
+		e2
+		e3
 	`)
 
 	// TODO: remove
